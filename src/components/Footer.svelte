b407a89babed14760f1fd85b75e3c4b98c121e4b
sara <sara.ekmanMedieinstitutet@gmail.com>
2022-05-25 01:07:49 +0200

    install svelte

---
+++ src/components/Footer.svelte
@@ -0,0 +1,21 @@
+<script>
+
+</script>
+
+<footer>
+<div class="copyright">copyright</div>
+</footer>
+
+<style>
+ footer {
+text-align: center;
+padding: 40px;
+    }
+    .copyright {
+        color: #aaa;
+        font-size: 14px;
+        display: inline-block;
+        padding: 40px;
+        border-top: 1px solid #ddd;
+    }
+</style>
